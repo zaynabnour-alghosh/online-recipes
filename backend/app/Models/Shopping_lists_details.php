3e15f5ebea45f72f0fe99974c036aa1fbaea2eb3
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-19 14:39:28 +0300

    BE<> Created the rest of the models

---
+++ backend/app/Models/Shopping_lists_details.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shopping_lists_details extends Model
+{
+    use HasFactory;
+}
